d3eac67eb61e4af02f1c5c8a1c1824ddc1e2a61b
Mitesh Shah <mitesh@miteshshah.com>
2020-10-04 23:06:24 +0530

    feat: base call with hourly data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,53 @@
+import moment from "moment";
+import axios from "axios";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <div className="">Test</div>;
+const UserLocation = ({ weather }) => {
+  let time = moment.unix(weather.current.dt);
+  console.log(time);
+  return (
+    <div>
+      {weather.timezone} | {weather.timezone_offset}
+      <p>{weather.current.dt}</p>
+      <p>{time.fromNow()}</p>
+      <ul>
+        {weather.hourly.map((item) => {
+          var hourTime = moment.unix(item.dt);
+          return <li>{hourTime.fromNow()}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default function Home({ name, weather }) {
+  return (
+    <div className="">
+      Current temp is: {weather.current.temp}
+      <UserLocation weather={weather} />
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const API_KEY = process.env.OPENWEATHER_API_KEY;
+  const lat = "19.069979699999998";
+  const lon = "72.8397202";
+
+  let res = await axios({
+    method: "GET",
+    url: `https://api.openweathermap.org/data/2.5/onecall`,
+    params: {
+      lat: lat,
+      lon: lon,
+      exclude: "minutely,alerts",
+      appid: API_KEY,
+    },
+  });
+  let weather = res.data;
+  return {
+    props: {
+      weather: res.data,
+    },
+  };
 }
