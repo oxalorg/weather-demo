7bcfa1f7844e08646e27547c3dd28f0cd15b3095
Mitesh Shah <mitesh@miteshshah.com>
2020-10-05 21:49:03 +0530

    refactor: hourly list into a component

---
+++ components/HourlyList.jsx
@@ -0,0 +1,34 @@
+import styles from "./HourlyList.module.scss";
+
+const HourlyList = ({ hourly, selectCurrentTimeSlot, currentDt }) => {
+  return (
+    <div className={styles.timeSlot}>
+      <div className={styles.timeSlot__list}>
+        {hourly.map((item) => {
+          var hourTime = moment.unix(item.dt);
+          return (
+            <div
+              key={item.dt}
+              className={
+                currentDt === item.dt
+                  ? styles.timeSlot__item__active
+                  : styles.timeSlot__item
+              }
+              onClick={(e) => {
+                e.preventDefault();
+                selectCurrentTimeSlot(item.dt);
+              }}
+            >
+              <div className={styles.timeSlot__itemTime}>
+                {hourTime.format("h a")}
+              </div>
+              <div className={styles.timeSlot__itemTemp}>
+                {KelvinToCelcius(item.temp)} &deg;
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
--- components/Landing.jsx
+++ components/Landing.jsx
@@ -4,39 +4,7 @@ import { KelvinToCelcius } from "../utils";
 
 import styles from "./Landing.module.scss";
 import DateSelector from "./DateSelector.jsx";
-
-const HourlyList = ({ hourly, selectCurrentTimeSlot, currentDt }) => {
-  return (
-    <div className={styles.timeSlot}>
-      <div className={styles.timeSlot__list}>
-        {hourly.map((item) => {
-          var hourTime = moment.unix(item.dt);
-          return (
-            <div
-              key={item.dt}
-              className={
-                currentDt === item.dt
-                  ? styles.timeSlot__item__active
-                  : styles.timeSlot__item
-              }
-              onClick={(e) => {
-                e.preventDefault();
-                selectCurrentTimeSlot(item.dt);
-              }}
-            >
-              <div className={styles.timeSlot__itemTime}>
-                {hourTime.format("h a")}
-              </div>
-              <div className={styles.timeSlot__itemTemp}>
-                {KelvinToCelcius(item.temp)} &deg;
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
+import HourlyList from "./HourlyList.jsx";
 
 const UserCity = ({ timezone }) => {
   return <button className={styles.badge}>{timezone}</button>;
